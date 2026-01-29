f94d5e4412f046261fb6d7c246868b2ada703d64
wilful_lemma <227983600+wirewl-08o3@users.noreply.github.com>
2026-01-29 00:41:18 +0000

    feat(docs): add ui namespace pages, including one for ac, enable markdown plugins, and progress on getting started page
    - add: draw-circle, draw-rect, draw-pie, and get-car documentation pages
    - enable link validator, and math rendering
    - update docs landing page and site config/style
    - begin writing up the getting started guides page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,52 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightImageZoom from 'starlight-image-zoom';
-import starlightSidebarSwipe from 'starlight-sidebar-swipe';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightImageZoom from "starlight-image-zoom";
+import starlightSidebarSwipe from "starlight-sidebar-swipe";
+import starlightLinksValidator from "starlight-links-validator";
+import remarkMath from "remark-math";
+import rehypeMathJax from "rehype-mathjax";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'CSP Lua API Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/lint069/csp-lua-api-docs' }],
-			customCss: ['./src/css/style.css'],
-			plugins: [
-				starlightSidebarSwipe(),
-				starlightImageZoom(),
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Types',
-					autogenerate: { directory: 'types' },
-				},
-				{
-					label: 'ui',
-					autogenerate: { directory: 'ui' },
-				},
-			],
-		}),
-	],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathJax],
+  },
+  integrations: [
+    starlight({
+      title: "CSP Lua API Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/lint069/csp-lua-api-docs",
+        },
+      ],
+      customCss: ["./src/css/style.css"],
+      plugins: [
+        starlightSidebarSwipe(),
+        starlightImageZoom(),
+        starlightLinksValidator(),
+      ],
+      sidebar: [
+        {
+          label: "Start Here",
+          items: [{ label: "Getting Started", slug: "guides/getting-started" }],
+        },
+        {
+          label: "Types",
+          autogenerate: { directory: "types" },
+        },
+        {
+          label: "ac",
+          autogenerate: { directory: "ac" },
+        },
+        {
+          label: "ui",
+          autogenerate: { directory: "ui" },
+        },
+      ],
+    }),
+  ],
 });
